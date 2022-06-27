5c72c6bcd4a51e03e99ced1e48378db160b357d4
Thyago Mota <thyagomota@gmail.com>
2022-06-27 16:11:52 -0600

    updates

--- activity_07_random/MainActivity.kt
+++ activity_07_random/MainActivity.kt
@@ -2,6 +2,8 @@ package edu.msudenver.randomnumbergenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
 import kotlin.random.Random
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // TODO: get a reference to the textview
+        var txtRandomNumber: TextView = findViewById(R.id.txtRandomNumber)
 
         // TODO: write the for the "onClickListener"
         // 1a get a reference to the edittext "from"
@@ -21,6 +24,27 @@ class MainActivity : AppCompatActivity() {
         // 2b extract the "to" integer value
         // 3 generate a random number in "from" until "to" + 1
         // 4 update the textview
-        
+        txtRandomNumber.setOnClickListener {
+                val edtFrom: EditText = findViewById(R.id.edtFrom)
+                val numFrom = Integer.valueOf(edtFrom.text.toString())
+                val edtTo: EditText = findViewById(R.id.edtTo)
+                val numTo = Integer.valueOf(edtTo.text.toString())
+                val randomNumber = Random.nextInt(numFrom, numTo + 1)
+                txtRandomNumber.text = randomNumber.toString()
+            }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val txtRandomNumber: TextView = findViewById(R.id.txtRandomNumber)
+        val randomNumber = Integer.valueOf(txtRandomNumber.text.toString())
+        outState.putInt("randomNumber", randomNumber)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val txtRandomNumber: TextView = findViewById(R.id.txtRandomNumber)
+        txtRandomNumber.text = savedInstanceState.getInt("randomNumber").toString()
     }
 }
+
