bcc31a2d65a14a7b7e6619747951d15fc123a179
Thyago Mota <thyagomota@gmail.com>
2022-06-27 16:14:48 -0600

    updates

--- activity_11_reminder/MainActivity.kt
+++ activity_11_reminder/MainActivity.kt
@@ -19,12 +19,22 @@ class MainActivity : AppCompatActivity() {
 
         // TODO #1: use the persistency file to update the EditText
         val edt2Save: EditText = findViewById(R.id.edt2Save)
+        try {
+            edt2Save.setText(openFileInput(FILE_NAME).bufferedReader().useLines { lines ->
+                lines.fold("") { some, text -> "$some\n$text" }
+            })
+        }
+        catch (ex: FileNotFoundException) {
 
+        }
 
         // TODO #2: set the button's listener to save the EditText's content to persistency file
         val btnSave: Button = findViewById(R.id.btnSave)
         btnSave.setOnClickListener {
-
+            val text = edt2Save.text.toString()
+            openFileOutput(FILE_NAME, Context.MODE_PRIVATE).bufferedWriter().use {
+                it.write(text)
+            }
         }
     }
 }
